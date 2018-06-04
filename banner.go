732a96a6f50d24efbf8de71a48ce90176f13a170
wi-cuckoo <yp_liuwei@163.com>
2018-06-04 22:40:17 +0800

    wtf

---
+++ banner.go
@@ -0,0 +1,21 @@
+// thank to http://patorjk.com/software/taag
+
+package main
+
+import (
+	"fmt"
+)
+
+const banner = `
+    _______           ________     
+   / ____(_)_______  / ____/ /_  __
+  / /_  / / ___/ _ \/ /_  / / / / /
+ / __/ / / /  /  __/ __/ / / /_/ / 
+/_/   /_/_/   \___/_/   /_/\__, /  
+                          /____/  hello, my lord! 
+`
+
+func main() {
+	fmt.Println("\033[H\033[2J")
+	fmt.Println(banner)
+}
