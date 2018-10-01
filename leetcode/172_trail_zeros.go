22be65f65e077542e366446479a36f811161be14
wi-cuckoo <yp_liuwei@163.com>
2018-10-01 22:33:31 +0800

    add new

---
+++ leetcode/172_trail_zeros.go
@@ -0,0 +1,43 @@
+/*
+给定一个整数 n，返回 n! 结果尾数中零的数量。
+
+示例 1:
+
+输入: 3
+输出: 0
+解释: 3! = 6, 尾数中没有零。
+示例 2:
+
+输入: 5
+输出: 1
+解释: 5! = 120, 尾数中有 1 个零.
+说明: 你算法的时间复杂度应为 O(log n) 。
+*/
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println(trailingZeroes(3))
+	fmt.Println(trailingZeroes(5))
+	fmt.Println(trailingZeroes(30))
+}
+
+// cacl count of five
+func trailingZeroes(n int) int {
+	cnt := 0
+	for i := 0; i <= n; i += 5 {
+		if i >= 5 {
+			for x := i / 5; x > 0; x = x / 5 {
+				cnt++
+				if x%5 != 0 {
+					break
+				}
+			}
+		}
+	}
+	return cnt
+}
