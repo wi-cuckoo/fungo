0d8dfdbecc8cf792ed5ea0863f645e5af308639d
刘伟3 <liuwei3@douyu.tv>
2018-12-24 19:43:33 +0800

    add bfs & dfs of tree model

--- model/model_test.go
+++ model/model_test.go
@@ -15,3 +15,49 @@ func TestHeap(t *testing.T) {
 		t.Error("should be 1, not", val)
 	}
 }
+
+func TestBFS(t *testing.T)  {
+	tree := &TreeNode{
+		Val:3,
+		Left: &TreeNode{
+			Val:9,
+		},
+		Right:&TreeNode{
+			Val:20,
+			Left:&TreeNode{
+				Val:17,
+			},
+			Right: &TreeNode{
+				Val:5,
+			},
+		},
+	}
+	arr := tree.BFS()
+	if len(arr) != 5 {
+		t.Fatal("length of tree should be 5")
+	}
+	t.Log(arr)
+}
+
+func TestDFS(t *testing.T)  {
+	tree := &TreeNode{
+		Val:3,
+		Left: &TreeNode{
+			Val:9,
+		},
+		Right:&TreeNode{
+			Val:20,
+			Left:&TreeNode{
+				Val:17,
+			},
+			Right: &TreeNode{
+				Val:5,
+			},
+		},
+	}
+	arr := tree.DFS()
+	if len(arr) != 5 {
+		t.Fatal("length of tree should be 5")
+	}
+	t.Log(arr)
+}
--- model/tree.go
+++ model/tree.go
@@ -8,11 +8,46 @@ type TreeNode struct {
 }
 
 // 实现广度优先搜索
-func (t *TreeNode) BFS() {
-
+func (t *TreeNode) BFS() []int {
+	if t == nil {
+		return []int{}
+	}
+	nodes := []int{}
+	queue := []*TreeNode{t} // 使用堆列来协助遍历
+	for len(queue) > 0 {
+		l := len(queue)
+		for i := 0; i <l; i++ {
+			nodes = append(nodes, queue[i].Val)
+			if queue[i].Left != nil {
+				queue = append(queue, queue[i].Left)
+			}
+			if queue[i].Right != nil {
+				queue = append(queue, queue[i].Right)
+			}
+		}
+		queue = queue[l:]
+	}
+	return nodes
 }
 
 // 实现深度优先搜索
-func (t *TreeNode) DFS() {
-
+func (t *TreeNode) DFS() []int {
+ 	if t == nil {
+ 		return []int{}
+	}
+	nodes := []int{}
+	stack := []*TreeNode{t}
+	for len(stack) > 0 {
+		// 父节点出栈
+		p := stack[len(stack)-1]
+		stack = stack[0:len(stack)-1]
+		nodes = append(nodes, p.Val)
+		if p.Right != nil {
+			stack = append(stack, p.Right)
+		}
+		if p.Left != nil {
+			stack = append(stack, p.Left)
+		}
+	}
+	return nodes
 }
