4314684ce556911fbc54d7d6d2e0992ae35eaa1d
刘伟3 <liuwei3@douyu.tv>
2019-01-17 14:02:31 +0800

    fuck update

--- algorithms_intro/sort/shuffle.go
+++ algorithms_intro/sort/shuffle.go
@@ -1,11 +1,18 @@
 package sort
 
-import (
-	"math/rand"
-)
+import "math/rand"
 
 func shuffle(nums []int) {
-	rand.Shuffle(len(nums), func(i, j int) {
+	// 使用官方的shuffle
+	/*
+		rand.Shuffle(len(nums), func(i, j int) {
+			nums[i], nums[j] = nums[j], nums[i]
+		})
+	*/
+
+	// 仿官方实现
+	for i := len(nums) - 1; i > 0; i-- {
+		j := rand.Intn(i + 1)
 		nums[i], nums[j] = nums[j], nums[i]
-	})
+	}
 }
