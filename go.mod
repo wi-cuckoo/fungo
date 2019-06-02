54bb923ba189dd717c6186fa85057099e57c349c
wi-cuckoo <yp_liuwei@163.com>
2019-06-02 17:46:55 +0800

    fuck

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/containerd/cgroups v0.0.0-20190328223300-4994991857f9
 	github.com/coreos/go-systemd v0.0.0-20190321100706-95778dfbb74e // indirect
 	github.com/docker/go-units v0.3.3 // indirect
+	github.com/gin-gonic/gin v1.4.0
 	github.com/godbus/dbus v0.0.0-20190413140323-8e900ab0295c // indirect
 	github.com/gogo/protobuf v1.2.1 // indirect
 	github.com/opencontainers/runtime-spec v0.1.2-0.20190408193819-a1b50f621a48
--- main.go
+++ main.go
@@ -1,21 +1,59 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"os"
 	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/wi-cuckoo/fungo/pubsub"
 )
 
-var pid int
-
-func init() {
-	pid = os.Getpid()
+func ping() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080
 }
 
 func main() {
-	select {
-	case <-time.After(time.Second * 5):
-	case <-time.After(time.Second * 2):
-		fmt.Println("timeout")
+	rn := pubsub.NewRegNotify()
+
+	go func() {
+		for i := 0; ; i++ {
+			err := rn.Issue(context.Background(), "test-portal", fmt.Sprintf("G1 fuck %d", i))
+			if err != nil {
+				fmt.Println("G1 issue payload err:", err.Error())
+				break
+			}
+			// time.Sleep(time.Millisecond)
+		}
+	}()
+
+	go func() {
+		for i := 0; ; i++ {
+			err := rn.Issue(context.Background(), "test-portal", fmt.Sprintf("G2 fuck %d", i))
+			if err != nil {
+				fmt.Println("G2 issue payload err:", err.Error())
+				break
+			}
+		}
+		time.Sleep(time.Millisecond)
+	}()
+
+	ti := time.NewTimer(time.Second * 2)
+	ch, _ := rn.Register(context.Background(), "test-portal")
+	defer rn.Unregister(context.Background(), "test-portal")
+	for {
+		select {
+		case p, ok := <-ch:
+			fmt.Println(p, ok)
+		case <-ti.C:
+			fmt.Println("time to shutdown pipe")
+			return
+		}
 	}
 }
