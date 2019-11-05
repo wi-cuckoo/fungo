73c674fe64fe3b1b266532e46914198d4c7fb085
wi-cuckoo <yp_liuwei@163.com>
2019-11-06 00:21:40 +0800

    feat: bit map

--- algorithms_intro/bit_op.go
+++ algorithms_intro/bit_op.go
@@ -5,7 +5,7 @@ import (
 )
 
 func main() {
-	fmt.Println(oddOne(1 << 32))
+	bits()
 }
 
 // cacl count of 1 is odd or not
@@ -29,4 +29,26 @@ func oddOne(x int) int {
 // cacl count of 1
 func countOne(x int) int {
 	return 0
+}
+
+func bits() {
+	bs := [125e5]byte{}
+	nums := []int{124e5, 133e4 + 1, 133e4 + 2, 10001}
+	for _, n := range nums {
+		idx, m := n/8, n%8
+		b := bs[idx]
+		bs[idx] = (1 << uint(m)) | b
+		// fmt.Println(idx, bs[idx])
+	}
+	for i, b := range bs {
+		if b == 0 {
+			continue
+		}
+		// fmt.Println(i, b)
+		for j := 0; j < 8; j++ {
+			if m := b >> uint(j); m&1 == 1 {
+				fmt.Println(i*8 + j)
+			}
+		}
+	}
 }
