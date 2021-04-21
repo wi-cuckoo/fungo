5c80f8b7ffb69d81a3bebe58f829233447a54d9b
wi-cuckoo <yp_liuwei@163.com>
2021-04-21 21:32:46 +0800

    fuck

--- leetcode/zero/91_numDecodings.go
+++ leetcode/zero/91_numDecodings.go
@@ -28,25 +28,15 @@ func numDecodings(s string) int {
 	if s == "" {
 		return 0
 	}
-	if s[0] == '0' {
-		return 0
-	}
 	dp := make([]int, len(s)+1)
-	dp[0], dp[1] = 1, 1
-
-	for i := 1; i < len(s); i++ {
-		if s[i] == '0' && (s[i-1] > '2' || s[i-1] == '0') {
-			return 0
+	dp[0] = 1
+	for i := 1; i <= len(s); i++ {
+		if s[i-1] != '0' {
+			dp[i] += dp[i-1]
 		}
-		if s[i] == '0' || s[i-1] == '0' {
-			dp[i+1] = dp[i-1]
-			continue
+		if i > 1 && s[i-2] != '0' && ((s[i-2]-'0')*10+(s[i-1]-'0') <= 26) {
+			dp[i] += dp[i-2]
 		}
-		if s[i-1:i+1] <= "26" {
-			dp[i+1] = dp[i-1] + dp[i]
-			continue
-		}
-		dp[i+1] = dp[i]
 	}
 	return dp[len(s)]
 }
