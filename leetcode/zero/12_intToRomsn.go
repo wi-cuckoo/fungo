0bf480893eb9d803ee942106f2113cb06546e247
wi-cuckoo <yp_liuwei@163.com>
2021-05-16 22:35:58 +0800

    fuck

---
+++ leetcode/zero/12_intToRomsn.go
@@ -0,0 +1,71 @@
+package zero
+
+/*
+罗马数字包含以下七种字符： I， V， X， L，C，D 和 M。
+
+字符          数值
+I             1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000
+例如， 罗马数字 2 写做 II ，即为两个并列的 1。12 写做 XII ，即为 X + II 。 27 写做  XXVII, 即为 XX + V + II 。
+
+通常情况下，罗马数字中小的数字在大的数字的右边。但也存在特例，例如 4 不写做 IIII，而是 IV。数字 1 在数字 5 的左边，所表示的数等于大数 5 减小数 1 得到的数值 4 。同样地，数字 9 表示为 IX。这个特殊的规则只适用于以下六种情况：
+
+I 可以放在 V (5) 和 X (10) 的左边，来表示 4 和 9。
+X 可以放在 L (50) 和 C (100) 的左边，来表示 40 和 90。
+C 可以放在 D (500) 和 M (1000) 的左边，来表示 400 和 900。
+给你一个整数，将其转为罗马数字。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/integer-to-roman
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+func intToRoman(num int) string {
+	var common = map[int]string{
+		1:    "I",
+		5:    "V",
+		10:   "X",
+		50:   "L",
+		100:  "C",
+		500:  "D",
+		1000: "M",
+		4:    "IV",
+		9:    "IX",
+		40:   "XL",
+		90:   "XC",
+		400:  "CD",
+		900:  "CM",
+	}
+	if s, ok := common[num]; ok {
+		return s
+	}
+	ans := ""
+	for _, step := range []int{1000, 100, 10, 1} {
+		n := num / step
+		num -= n * step
+
+		if s, ok := common[n*step]; ok {
+			ans += s
+			continue
+		}
+		if n > 5 {
+			ans += common[5*step]
+			n -= 5
+		}
+		ans += nstr(common[step], n)
+	}
+	return ans
+}
+
+func nstr(one string, n int) string {
+	ans := ""
+	for ; n > 0; n-- {
+		ans += one
+	}
+	return ans
+}
